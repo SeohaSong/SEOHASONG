5eb72822474c3308108cef257d2d5eabcdbada77
seohasong <tisutoo@gmail.com>
2020-06-14 11:07:09 +0900

    continue previous working

--- cmd/sr/load/main.sh
+++ cmd/sr/load/main.sh
@@ -3,6 +3,14 @@ echo $@
 echo $( eval $INTRO )
 
 seohasong
+
+(
+    cd data/SEOHASONG
+    cat env/KEY
+    echo
+    git pull origin master
+)
+
 mv data/boilerplate/.git data/SEOHASONG/boilerplate
 rm -r data/boilerplate
 mv data/SEOHASONG/boilerplate data
