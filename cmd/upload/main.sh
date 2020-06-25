1b7b56f084a144121848c73bf4c3e4c4c91d2762
seohasong <tisutoo@gmail.com>
2020-06-26 06:30:06 +0900

    continue previous working

--- cmd/upload/main.sh
+++ cmd/upload/main.sh
@@ -1,10 +1,15 @@
-if [[ -d .git && $PWD != $DDD_PATH ]]
-then
+_upload()
+{
     git config user.email "tisutoo@gmail.com"
     git config user.name "seohasong"
     git add .
-    git commit -m "continue previous working"
+    git commit -m "continue previous working" || :
     git push
+}
+
+if [[ -d .git && $PWD != $DDD_PATH ]]
+then
+    _upload
     return
 fi
 
@@ -16,9 +21,5 @@ do (
     if [ ! -d $apath ]; then continue; fi
     $DDD echo Update \'$apath\' repo
     cd $apath
-    git config user.email "tisutoo@gmail.com"
-    git config user.name "seohasong"
-    git add .
-    git commit -m "continue previous working" || :
-    git push
+    _upload
 ) done
