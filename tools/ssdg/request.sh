e83983cbd45ccec4dd5ba3f2f6457237994fe718
seohasong <tisutoo@gmail.com>
2021-02-06 23:36:57 +0900

    continue previous working

--- tools/ssdg/request.sh
+++ tools/ssdg/request.sh
@@ -1,10 +1,24 @@
 json='[
-    "get-acct",
-    {}
+    "cu-usr-sool",
+    {
+        "usr-info": {
+            "usr-id": 0,
+            "token": "asdf",
+            "email": "soojak.korea@gmail.com"
+        },
+        "sool-id": 0,
+        "cert": null,
+        "like": null,
+        "taste": null,
+        "star": null,
+        "review": null,
+        "pairing": null,
+        "scent": null
+    }
 ]'
 
 curl \
     -X POST \
     -H "Content-Type: application/json" \
     -d "$json" \
-    localhost:8000
+    http://ec2-3-36-128-139.ap-northeast-2.compute.amazonaws.com:8000
