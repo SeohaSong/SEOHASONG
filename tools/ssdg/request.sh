8d6026b96a56d7ab194e81c89262c462350c3a3b
seohasong <tisutoo@gmail.com>
2021-02-17 02:50:51 +0900

    continue previous working

--- tools/ssdg/request.sh
+++ tools/ssdg/request.sh
@@ -2,7 +2,7 @@ json='[
     "cu-usr-sool",
     {
         "usr-info": {
-            "usr-id": 0,
+            "usr-id": 1,
             "token": "asdf",
             "email": "soojak.korea@gmail.com"
         },
@@ -17,8 +17,16 @@ json='[
     }
 ]'
 
+
+
+
+bgn_time=$( date +%s%N )
+
 curl \
     -X POST \
     -H "Content-Type: application/json" \
     -d "$json" \
     http://ec2-3-36-128-139.ap-northeast-2.compute.amazonaws.com:8000
+echo
+
+echo $(( $( date +%s%N ) - $bgn_time ))
