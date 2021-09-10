c60de3ea7a23ffe7d6c5fdd1277bcf928b53cd00
seohasong <tisutoo@gmail.com>
2021-09-10 10:44:43 +0900

    continue previous working

--- cmd/update/boil/main.sh
+++ cmd/update/boil/main.sh
@@ -1,18 +1,4 @@
-echo
 echo seoha-song
 cat $DDD_PATH/env/SR-KEY
-echo
-
-nxt_path=$DDD_PATH/data/boilerplate_
-git clone https://github.com/seoha-song/boilerplate.git $nxt_path
-
-cd $DDD_PATH/data/boilerplate
-mv .git $DDD_PATH/data/git
-mv $nxt_path/.git .git
-rm -rf $nxt_path
-
-git submodule deinit --all -f || :
-git add .
-git reset --hard HEAD
-rm -rf .git
-mv $DDD_PATH/data/git .git
+git clone https://github.com/seoha-song/boilerplate.git \
+          $DDD_PATH/data/boilerplate
