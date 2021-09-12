81498db6b8b5089c923d58e943252d60385144ff
seohasong <tisutoo@gmail.com>
2021-09-12 23:40:14 +0900

    continue previous working

--- cmd/update/boil/main.sh
+++ cmd/update/boil/main.sh
@@ -1,4 +1,5 @@
+tgt=$DDD_PATH/data/boilerplate
+cd $tgt
 echo seoha-song
 cat $DDD_PATH/env/SR-KEY
-git clone https://github.com/seoha-song/boilerplate.git \
-          $DDD_PATH/data/boilerplate
+git clone https://github.com/seoha-song/boilerplate.git $tgt
